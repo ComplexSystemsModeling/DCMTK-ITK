15604c28976feb389b0836728e2c78f88a850a90
Alex Gouaillard <agouaillard@gmail.com>
2011-12-05 12:29:08 +0800

    ENH: source files, BUG: wrong module name prevented compilation
    Change-Id: I449217be9752d11552cc1d7646e29cffb3323952

--- Modules/IO/DCMTK/itk-module.cmake
+++ Modules/IO/DCMTK/itk-module.cmake
@@ -1,13 +1,11 @@
 set(DOCUMENTATION "This module contains the third party <a
 href=\"http://dicom.offis.de/dcmtk/\">DCMTK</a> DCMTK is a collection of libraries and applications implementing large parts the DICOM standard.")
 
-itk_module(ITKDCMTK
+itk_module(ITKIODCMTK
   DEPENDS
-     ITKDCMTK
-     ITKIOBase
+     ITKIOImageBase
   TEST_DEPENDS
-     ITKTestKernel
-     ITKImageIntensity
+    ITKTestKernel
   DESCRIPTION
     "${DOCUMENTATION}"
 )
